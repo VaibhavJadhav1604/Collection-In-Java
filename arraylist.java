a11b219d52fb619e9ed1430781a0b4546090aca4
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-09-22 20:28:11 +0530

    Program to Demonstrate ArrayList

---
+++ arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+class ArrayListDemo 
+{
+ public static void main(String[] args)
+ {
+ // creating an Array List named colors
+   ArrayList Al = new ArrayList();
+ // add elements in the Array List
+Al.add("Red");
+Al.add(5);
+Al.add("Null");
+Al.add("Orange");
+Al.add("Red");
+ // printing the ArrayList
+System.out.println(Al);
+ }
+}
